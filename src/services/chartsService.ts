48527a616505819aa1030103ccdc1c1e62934b33
Rafael Souza <rafaelsprata@outlook.com>
2025-06-25 01:11:05 -0300

    remove código comentado e simplifica a função openCharts para abrir o gráfico em uma nova aba

--- src/services/chartsService.ts
+++ src/services/chartsService.ts
@@ -2,24 +2,42 @@ import { Artist } from '@/interfaces/Artist';
 import axios from 'axios';
 
 export default {
-    async openCharts(color:string, image:string, rank:Artist[]){
-        try{
-            const response = await axios.post(`https://charts-edition.onrender.com/main`, 
-                { color, image, rank},
-                { responseType: 'blob' }
-            );
-            const blob = response.data;
-            const url = URL.createObjectURL(blob);
+    // async openCharts(color:string, image:string, rank:Artist[]){
+    //     try{
+    //         const response = await axios.post(`https://charts-edition.onrender.com/main`, 
+    //             { color, image, rank},
+    //             { responseType: 'blob' }
+    //         );
+    //         const blob = response.data;
+    //         const url = URL.createObjectURL(blob);
 
-            const link = document.createElement('a');
-            link.href = url;
-            link.download = 'charts.png';
-            document.body.appendChild(link); // ⬅️ necessário no mobile
-            link.click();
-            document.body.removeChild(link); // limpa após o clique
-            URL.revokeObjectURL(url); // libera memória
-        } catch(error){
-            throw new Error(`Erro: ${error}`);
+    //         const link = document.createElement('a');
+    //         link.href = url;
+    //         link.download = 'charts.png';
+    //         document.body.appendChild(link); // ⬅️ necessário no mobile
+    //         link.click();
+    //         document.body.removeChild(link); // limpa após o clique
+    //         URL.revokeObjectURL(url); // libera memória
+    //     } catch(error){
+    //         throw new Error(`Erro: ${error}`);
+    //     }
+    // },
+
+    async openCharts(color: string, image: string, rank: Artist[]) {
+        const newTab = window.open('', '_blank');
+
+        try {
+        const response = await axios.post(`https://charts-edition.onrender.com/main`, 
+            { color, image, rank },
+            { responseType: 'blob' }
+        );
+        const blob = response.data;
+        const url = URL.createObjectURL(blob);
+
+        if (newTab) newTab.location.href = url;
+        } catch (error) {
+        if (newTab) newTab.close();
+        console.error(error);
         }
     }
 };
